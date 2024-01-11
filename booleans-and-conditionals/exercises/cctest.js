da4ececde3232788534f4f989ae057e2dab9df20
Curtiss <cmcrable@gmail.com>
2024-01-10 22:47:29 -0600

    Crable - Conditionals LC Ch 5 Exercises

---
+++ booleans-and-conditionals/exercises/cctest.js
@@ -0,0 +1,7 @@
+let num = 6;
+
+if (num % 2 === 0) {
+    if (num % 2 === 1) {
+        console.log("odd");
+    }
+}
--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,5 +1,13 @@
 // Declare and initialize the variables for exercise 1 here:
 
+let engineIndicatorLight = "red blinking";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
+
+
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
 if (engineIndicatorLight === "green") {
@@ -9,3 +17,9 @@ if (engineIndicatorLight === "green") {
 } else {
    console.log("engines are off");
 }
+
+/*
+Since the first 'if' statement is false, line 14 will be skipped and not execute.
+Since the 'else if' statement is false, line 16 will be skipped and not execute.
+Since the 'else' statement has no other condition, it will evaluate as true and the console will print "engines are off".
+*/
