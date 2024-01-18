f8917dc5543d473b89647117d6404fc64f6caa12
Curtiss <cmcrable@gmail.com>
2024-01-18 16:24:47 -0600

    Crable - Class 3 Arrays LC Ch 8 Exercise

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,10 +2,32 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
+//The parameter inside the parentheses defines the delimiter used to separate the string into array indexes...
+console.log(str.split());       // when no parameter is defined, the entire string is placed as a single index inside the array
+console.log(str.split('e'));    // the 'e' parameter splits the string into a new index in the array wherever an 'e' is present; the character is used to split and therefore is excluded
+console.log(str.split(' '));    // the ' ' (space) parameter splits the string into a new index in the array wherever a space is present, effectively splitting the string by words
+console.log(str.split(''));     // the '' (null) parameter splits the string into a new index in the array by character
 
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
 
-//3) Do split or join change the original string/array?
+console.log(arr.join());        // when no parameter is defined the array elements are placed in a string separated by comma connectors
+console.log(arr.join('a'));     // the 'a' parameter joins the array elements into a string by the that connector, effectively creating a new string
+console.log(arr.join(' '));     // the ' ' (space) parameter joins the array elements into a string separated by that space connector
+console.log(arr.join(''));      // the '' (null) parameter joins the array elements into a string with no connectors
+
+//3) Do split or join change the original string/array? <<NO>>
+
+console.log(str);
+console.log(arr);
 
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+
+let newArray = cargoHold.split(',');
+console.log(newArray);
+newArray.sort();
+console.log(newArray.join(', '));
+
+// or do the same thing using a single chained statement:
+
+console.log(cargoHold.split(',').sort().join(', '));
--- arrays/exercises/part-six-arrays.js
+++ arrays/exercises/part-six-arrays.js
@@ -2,10 +2,40 @@
 
 //1) Define and initialize the arrays specified in the exercise to hold the name, chemical symbol and mass for different elements.
 
+let element1 = ['hydrogen', 'H', 1.008];
+let element2 = ['helium', 'He', 4.003];
+let element26 = ['iron', 'Fe', 55.85];
+
 //2) Define the array 'table', and use 'push' to add each of the element arrays to it. Print 'table' to see its structure.
 
+let table = [];
+table.push(element1);
+table.push(element2);
+table.push(element26);
+
+console.log(table);
+
 //3) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
+// Printing the table with a single index references the outer array, and diplays the whole table
+// Printing the table with 2 indices displays the specified array of the inner index of the specified outer array index
+console.log(table[1]);
+console.log(table[1][1]);
 
 //4) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26.
+console.log(`The mass of element1 is ${table[0][2]}.`);
+console.log(`The name of element2 is ${table[1][0]}.`);
+console.log(`The symbol for element26 is ${table[2][1]}.`);
 
 //5) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
+
+let state1 = ['gas'];
+let state2 = ['noble gas'];
+let state3 = ['solid'];
+
+element1.push(state1);
+element2.push(state2);
+element26.push(state3);
+
+console.log(table);
+
+console.log(table[1][1][0]);
