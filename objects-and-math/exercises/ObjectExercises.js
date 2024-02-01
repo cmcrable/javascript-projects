1d40e52a5be9b179310f7d057e831b146bc00df1
Curtiss <cmcrable@gmail.com>
2024-02-01 17:28:57 -0600

    Crable Class 7 Ch 12 Objects and the Math Object Exercise - IC

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -15,7 +15,44 @@ let salamander = {
 
 // After you have created the other object literals, add the astronautID property to each one.
 
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6
+};
+
+let beagle = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 1
+};
+
+let tardigrade = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1
+};
+
 // Create an array to hold the animal objects.
+
+let animalHouse = ['superChimpOne', 'salamander','superChimp2','beagle','tardigrade'];
+
+function spaceName(animal) {
+   animal.astronautID = Math.trunc(Math.random(11)*10);
+   return animal;
+}
+
+function assignID(arr) {
+   for (let i = 0; i < arr.length; i++) {
+      spaceName(arr[i].astronautID);
+   }
+   console.log(animalHouse);
+};
+
+assignID(animalHouse);
 
 // Print out the relevant information about each animal.
 
