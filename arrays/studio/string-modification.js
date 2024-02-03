d89aa1f8df73669e4f1892c60a0ea99d519f208f
Curtiss <cmcrable@gmail.com>
2024-02-03 13:35:37 -0600

    Crable Studio 3 IC

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -4,6 +4,15 @@ let str = "LaunchCode";
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
 
+
+let oneStr = str.replace("Lau", "");
+let twoStr = str.slice(0 ,3);
+
+let finalStr = oneStr + twoStr
+
+console.log(`The originial string is "${str}" and the modified string is "${finalStr}".`);
+
+
 //Use a template literal to print the original and modified string in a descriptive phrase.
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
