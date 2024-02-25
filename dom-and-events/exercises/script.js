1992a048306c635bf30250028e142e9c59bc4476
Curtiss <cmcrable@gmail.com>
2024-02-24 22:43:03 -0600

    Crable Ch23 Exercises - The DOM and Events - CP

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,24 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", function() {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+    missionAbort.addEventListener("mouseover", function(event) {
+        event.target.style.backgroundColor = 'red';
+    });
+
+    missionAbort.addEventListener("mouseout", function(event) {
+        event.target.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener("click", function() {
+        let abortConfirmation = window.confirm("Are you sure you want to abort the mission?");
+        if (abortConfirmation) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home.";
+        }
+    })
 }
 
 window.addEventListener("load", init);
