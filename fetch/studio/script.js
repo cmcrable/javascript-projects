0d0fd6caa89b50a71970e7acc05d11c12dbb6b43
Curtiss <cmcrable@gmail.com>
2024-03-04 20:40:44 -0600

    Crable Ch27 Class 16 Studio - Fetch & JSON - CP

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,25 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function(){
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response){
+                response.json().then( function(json){
+                    const container = document.getElementById("container");
+                    for (let i = 0; i < json.length; i++){
+                        container.innerHTML += `
+                        <div class="astronaut">
+                            <div class="bio">
+                                <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                                <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li>Active: <span style="color:${json[i].active ? "green" : "red"}">${json[i].active}</span></li>
+                                <li>Skills: ${json[i].skills}</li>
+                                </ul>
+                            </div>
+                            <img class="avatar" src="${json[i].picture}">
+                        </div>`;
+                    };
+ 
+                });
+
+});
+});
